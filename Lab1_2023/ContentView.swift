8199c339787c2b4ba8048ceea1248a968342a25a
jamespaologarcia <james.paolo.p.garcia@gmail.com>
2023-01-11 15:18:35 -0800

    New icon

--- Lab1_2023/ContentView.swift
+++ Lab1_2023/ContentView.swift
@@ -11,7 +11,8 @@ struct ContentView: View {
     @State private var description = ""
     var body: some View {
         VStack {
-            Image(systemName: "ladybug")
+            Image(systemName: "square.and.arrow.up")
+                .resizable(resizingMode: .stretch)
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             TextEditor(text: $description)
