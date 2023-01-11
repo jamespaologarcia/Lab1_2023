e790bfaaba5d8f772cad67d7be22ea574132a0cd
jamespaologarcia <james.paolo.p.garcia@gmail.com>
2023-01-11 15:43:08 -0800

    Lab2 Part 1

--- Lab1_2023/ContentView.swift
+++ Lab1_2023/ContentView.swift
@@ -9,12 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var description = ""
+    @State private var favorite = false
     var body: some View {
         VStack {
             Image(systemName: "square.and.arrow.up")
                 .resizable(resizingMode: .stretch)
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
+                .background(favorite ? Color.yellow : Color.white)
+            Toggle(isOn: $favorite) {
+                Text("Favorite")
+            }
             TextEditor(text: $description)
         }
         .padding()
